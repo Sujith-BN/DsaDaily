ac29428b6ec9a5f67f0bc8bda6324fe22015dc2a
Sujith-BN <sujithbn01@gmail.com>
2025-10-30 23:20:05 +0530

    10/30/2025

---
+++ src/KthHighest.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+public class KthHighest {
+    class Solution {
+        public int findKthLargest(int[] nums, int k) {
+            PriorityQueue<Integer> heap = new PriorityQueue<>();
+            for (int num : nums) {
+                heap.offer(num);
+                if (heap.size() > k) {
+                    heap.poll();
+                }
+            }
+            return heap.peek();
+        }
+    }
+
+}
