e0d91931b876b50b61fc162844a4a071c74978cf
Sujith-BN <sujithbn01@gmail.com>
2025-11-05 12:27:47 +0530

    11/5/2025

---
+++ src/LargestNumber.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class LargestNumber {
+        public String largestNumber(int[] nums) {
+            String[] strArr = new String[nums.length];
+            for(int i =0 ; i < nums.length ; i++){
+                strArr[i] = String.valueOf(nums[i]);
+
+            }
+
+            Arrays.sort(strArr,
+                    (a,b)->(b+a).compareTo(a+b));
+
+            if(strArr[0].equals("0")){
+                return "0";
+            }
+
+            return String.join("",strArr);
+
+        }
+    }
+
