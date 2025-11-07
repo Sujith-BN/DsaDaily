e3c20e22720fde2e5d7b3c95d19a0d9917b87864
Sujith-BN <sujithbn01@gmail.com>
2025-11-07 21:48:08 +0530

    11/7/2025

---
+++ src/SingleNum.java
@@ -0,0 +1,12 @@
+public class SingleNum {
+        public int singleNumber(int[] nums) {
+
+            int xor = 0;
+            for(int x :nums){
+                xor^=x;
+            }
+            return xor;
+
+        }
+    }
+
