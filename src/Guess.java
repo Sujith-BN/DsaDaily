034e0a7a4a4c69e80cddb48c1482ed8091c16b35
Sujith-BN <sujithbn01@gmail.com>
2025-11-16 23:19:40 +0530

    11/16/2025

---
+++ src/Guess.java
@@ -0,0 +1,16 @@
+public class Guess {
+
+        public int guessNumber(int n) {
+            int left = 1;
+            int right = n;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                int res = guess(mid);
+                if (res == 0) return mid;
+                else if (res < 0) right = mid - 1;
+                else left = mid + 1;
+            }
+            return -1;
+        }
+    }
+
