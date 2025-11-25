6d64f4ae0fd1ffc7e0560c39727693f2df6fb419
Sujith-BN <sujithbn01@gmail.com>
2025-11-26 00:17:43 +0530

    11/26/2025

---
+++ src/Intersection.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Intersection {
+        public int[] intersection(int[] nums1, int[] nums2) {
+            Arrays.sort(nums1);
+            Arrays.sort(nums2);
+
+            ArrayList<Integer> list = new ArrayList<>();
+
+            int i = 0, j = 0;
+
+            while (i < nums1.length && j < nums2.length) {
+                if (nums1[i] == nums2[j]) {
+
+                    if (list.size() == 0 || list.get(list.size() - 1) != nums1[i]) {
+                        list.add(nums1[i]);
+                    }
+                    i++;
+                    j++;
+                } else if (nums1[i] < nums2[j]) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+
+            int[] arr = new int[list.size()];
+            for (int k = 0; k < list.size(); k++) {
+                arr[k] = list.get(k);
+            }
+
+            return arr;
+        }
+    }
+
+
