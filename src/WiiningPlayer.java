1e8b3ca18d246497641d1a2d96ec5921d2868806
Sujith-BN <sujithbn01@gmail.com>
2025-11-27 23:16:20 +0530

    11/27/2025

---
+++ src/WiiningPlayer.java
@@ -0,0 +1,8 @@
+public class WiiningPlayer {
+        public String winningPlayer(int x, int y) {
+            int turns = Math.min(x, y / 4);
+            return turns % 2 == 1 ? "Alice" : "Bob";
+        }
+    }
+
+
